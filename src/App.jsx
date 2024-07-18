9c1fe396f834527fc58d3a96a343b2f3d08cc311
222112038 <falanarofako29@gmail.com>
2024-07-18 13:20:58 +0700

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
+import Beranda from "./pages/beranda";
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-500">
-        Hello, Vite + React + Tailwind CSS!
-      </h1>
-    </div>
+    <Beranda />
   );
 }
 
